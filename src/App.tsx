0a780e92954644a25028e1bdfb53f27275b82b49
Stian Gaustad <stian.gaustad@no.experis.com>
2024-05-21 15:39:37 +0200

    App - Added initial routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,36 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import HeaderComponent from "@/components/header/HeaderComponent";
+import { userContext } from './util/context';
+import { Navigate, Route, Routes } from 'react-router-dom';
+import LandingPage from './pages/LandingPage/LandingPage';
+import MovieOverviewPage from './pages/MovieOverview/MovieOverviewPage';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [user, setUser] = useState({})
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <userContext.Provider
+        value={{user: user, setUser: setUser}}
+      >
+
+        <HeaderComponent />
+        <Routes>
+          <Route 
+            path="/"
+            element={<LandingPage />}
+          />
+          <Route 
+            path="/movies/"
+            element={<MovieOverviewPage />}
+          />
+          <Route // Send unknown paths back to landing page
+            path="*"
+            element={<Navigate replace to="/" />}
+          />
+        </Routes>
+      </userContext.Provider>
     </>
   )
 }
