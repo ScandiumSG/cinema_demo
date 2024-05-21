fbf7debf7eb54c3aa410547d35f91461ea2cf3a5
Stian Gaustad <stian.gaustad@no.experis.com>
2024-05-21 15:55:59 +0200

    timeUtil - Changed time display format

--- src/util/timeUtils.ts
+++ src/util/timeUtils.ts
@@ -1,5 +1,5 @@
 export const timeCalculator = (timeInMinutes: number) => {
     const hours = Math.floor(timeInMinutes / 60);
     const minutes = timeInMinutes - 60 * hours
-    return hours+"hours "+minutes+"minutes"
+    return hours+":"+minutes.toString().padStart(2, "0");
 }
