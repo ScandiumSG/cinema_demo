b349b2e07c9e67d617ef8c2a08c224bcc0b1fe89
Stian Gaustad <stian.gaustad@no.experis.com>
2024-05-22 16:02:28 +0200

    Profile - Created placeholder profile page

---
+++ src/components/ProfileView/ProfileView.tsx
@@ -0,0 +1,11 @@
+import "./ProfileView.css"
+
+const ProfileView = () => {
+    return(
+        <div>
+            Placeholder
+        </div>
+    )
+}
+
+export default ProfileView;
---
+++ src/pages/ProfilePage/ProfilePage.tsx
@@ -0,0 +1,11 @@
+import "./ProfilePage.css"
+
+const ProfilePage = () => {
+    return(
+        <div>
+            Profile page placeholder
+        </div>
+    )
+}
+
+export default ProfilePage;
