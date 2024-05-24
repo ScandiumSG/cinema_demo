83efe2d40283e8753d3e3b3daffe5f1e8d55ee72
Stian Gaustad <stian.gaustad@no.experis.com>
2024-05-24 10:36:36 +0200

    Interface - Added interface for Screening, Theater, Seat

---
+++ src/interfaces/IScreening.ts
@@ -0,0 +1,10 @@
+import { IMovieDetails } from "./IMovie";
+import ITheater from "./ITheater";
+
+export interface IScreening {
+    id: number,
+    movie: IMovieDetails,
+    theater: ITheater,
+    tickets: [],
+    startTime: string
+}
