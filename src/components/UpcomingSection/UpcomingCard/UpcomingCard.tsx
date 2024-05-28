c46bf3cc22cb18234e090534fb205e8afa7a3602
Stian Gaustad <stian.gaustad@no.experis.com>
2024-05-28 12:48:38 +0200

    UpcomingCard - Added screening start time and made minor style adjustments

--- src/components/UpcomingSection/UpcomingCard/UpcomingCard.tsx
+++ src/components/UpcomingSection/UpcomingCard/UpcomingCard.tsx
@@ -1,6 +1,7 @@
 import { IScreening } from "@/interfaces/IScreening"
 import "./UpcomingCard.css"
 import MovieCard from "@/components/MovieCard/MovieCard"
+import { getTimeFromDateTimeString } from "@/util/timeUtils"
 
 interface IUpcomingCard {
     screening: IScreening,
@@ -11,9 +12,14 @@ const UpcomingCard: React.FC<IUpcomingCard> = ({screening}) => {
     return(
         <div className="upcoming-card-parent-container">
             <MovieCard movie={screening.movie} />
-            <span className="upcoming-card-theater-name">
-                Theater: {screening.theater.name}
-            </span>
+            <div className="upcoming-card-locale-details">
+                <p className="upcoming-card-start-time">
+                    {getTimeFromDateTimeString(screening.startTime)}
+                </p>
+                <span className="upcoming-card-theater-name">
+                    {screening.theater.name}
+                </span>
+            </div>
         </div>
     )
 }
