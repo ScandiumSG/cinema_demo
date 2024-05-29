b40db302f9d314527d1b4eb4ac68a5c873062048
Stian Gaustad <stian.gaustad@no.experis.com>
2024-05-29 14:50:31 +0200

    MoviePage - Added link from upcomingScreening component to screeningpage

--- src/components/UpcomingScreeningList/UpcomingScreeningGroup/UpcomingScreeningItem/UpcomingScreeningItem.tsx
+++ src/components/UpcomingScreeningList/UpcomingScreeningGroup/UpcomingScreeningItem/UpcomingScreeningItem.tsx
@@ -1,14 +1,24 @@
 import { IScreening } from "@/interfaces/IScreening";
 import "./UpcomingScreeningItem.css"
 import { getTimeFromDateTimeString } from "@/util/timeUtils";
+import { useNavigate } from "react-router-dom";
 
 interface IScreeningItem {
     screening: IScreening,
 }
 
 const UpcomingScreeningItem: React.FC<IScreeningItem> = ({screening}) => {
+    const navigate = useNavigate();
+    const navigateToScreening = () => {
+        const navString = `/screening/${screening.movie.id}/${screening.id}`
+        navigate(navString);
+    }
+
     return(
-        <div className="upcoming-screening-item clickable">
+        <div 
+            className="upcoming-screening-item clickable"
+            onClick={() => navigateToScreening()}
+        >
             <div>
                 <span>{getTimeFromDateTimeString(screening.startTime)} - </span>
                 <span>{screening.theater.name}</span>
