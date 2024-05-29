2440e7b9087492fe3d1615c039623ea3d75bcf30
Stian Gaustad <stian.gaustad@no.experis.com>
2024-05-29 16:03:14 +0200

    Util - Added localization currency standard and a ticket group array

---
+++ src/util/ticketUtils.ts
@@ -0,0 +1,22 @@
+export const ticketOptions  = [
+    {
+        name: "Adult",
+        price: 99,
+    },
+    {
+        name: "Student",
+        price: 49,
+    },
+    {
+        name: "Senior",
+        price: 59,
+    },
+    {
+        name: "Child",
+        price: 99,
+    }, 
+    {
+        name: "Toddler",
+        price: 399,
+    }
+]
