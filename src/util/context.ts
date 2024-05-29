479aca14acba50f2f57c13387125cad6efb13b74
Stian Gaustad <stian.gaustad@no.experis.com>
2024-05-29 16:02:45 +0200

    Context - Added a purchase and seatmap modal

--- src/util/context.ts
+++ src/util/context.ts
@@ -1,5 +1,6 @@
-import { IUserContext } from '@/interfaces/UserInterfaces';
 import { createContext } from 'react'
+import { IUserContext } from '@/interfaces/UserInterfaces';
+import { IPurchaseModalContext } from '@/interfaces/IScreening';
 
 export const userContext = createContext<IUserContext>({
     user: {},
@@ -7,3 +8,10 @@ export const userContext = createContext<IUserContext>({
     loginModal: false,
     showLoginModal: () => {}
 });
+
+export const purchaseModalContext = createContext<IPurchaseModalContext>({
+    showPurchase: false,
+    setShowPurchase: () => {},
+    showSeatingMap: false,
+    setShowSeatingMap: () => {},
+})
