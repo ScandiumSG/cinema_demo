c698c2e2ec63632a2e05b0f73e8c64ff990d3c53
Stian Gaustad <stian.gaustad@no.experis.com>
2024-06-06 14:50:16 +0200

    HeaderComponent - Added scaling and conditional rendering for screen width

--- src/components/header/HeaderComponent.tsx
+++ src/components/header/HeaderComponent.tsx
@@ -2,13 +2,18 @@ import { useContext, useEffect, useState } from "react";
 import LoginModal from "../common/loginModal/LoginModal";
 import "./HeaderComponent.css"
 import { IUserContext } from "@/interfaces/UserInterfaces";
-import { userContext } from "@/util/context";
+import { headerNavigationContext, userContext } from "@/util/context";
 import SmallHeader from "./SmallHeader/SmallHeader";
 import LargeHeader from "./LargeHeader/LargeHeader";
 
 const HeaderComponent = () => {
     const { loginModal } = useContext<IUserContext>(userContext);
     const [isDesktop, setDesktop] = useState<boolean>(window.innerWidth > 600);
+    const headerNavigationItems: {display: string, route: string}[] = [
+        //{display: "Frontpage", route: "/"}, 
+        {display: "Upcoming", route: "screening/upcoming"}, 
+        {display: "Movies", route: "movies"}
+    ];
 
     useEffect(() => {
         const handleWindowResize = () => {
@@ -27,16 +32,18 @@ const HeaderComponent = () => {
     }, [])
 
     return (
-        <>
-        {loginModal && <LoginModal />}
-        <header className="header-container">
-        {isDesktop ? 
-            <LargeHeader />
-        : 
-            <SmallHeader />
-        }
-        </header>
-        </>
+        <headerNavigationContext.Provider value={{
+            navItems: headerNavigationItems
+        }}>
+            {loginModal && <LoginModal />}
+            <header className="header-container">
+            {isDesktop ? 
+                <LargeHeader/>
+                : 
+                <SmallHeader/>
+            }
+            </header>
+        </headerNavigationContext.Provider>
     )
 }
 
