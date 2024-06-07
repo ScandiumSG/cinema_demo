ec6e7525581f74b19df1020c55bc58fd2bd8c1a0
Stian Gaustad <stian.gaustad@no.experis.com>
2024-06-07 10:47:47 +0200

    MovieCard - Added StarRating to the MovieCard

--- src/components/MovieCard/MovieCard.tsx
+++ src/components/MovieCard/MovieCard.tsx
@@ -2,6 +2,7 @@ import { timeCalculator } from "@/util/timeUtils";
 import "./MovieCard.css"
 import { IMovie } from "@/interfaces/IMovie";
 import { useNavigate } from "react-router-dom";
+import StarRating from "../common/StarRating/StarRating";
 
 interface Props {
     movie: IMovie
@@ -13,7 +14,7 @@ const MovieCard = ({movie}: Props) => {
     const handleCardInteraction = () => {
         navigate("/movies/"+movie.id)
     }
-    
+
     return(
         <div 
             className="movie-card-container"
@@ -21,7 +22,8 @@ const MovieCard = ({movie}: Props) => {
         >
             <h3 className="movie-card-title-header">{movie.title}</h3>
             <img src="https://image.tmdb.org/t/p/original/hkxxMIGaiCTmrEArK7J56JTKUlB.jpg" />
-            <div>
+            <div className="movie-card-additional-information">
+                <StarRating rating={movie.averageRating}/>
                 <span>Runtime: {timeCalculator(movie.runtime)}</span>
             </div>
         </div>
