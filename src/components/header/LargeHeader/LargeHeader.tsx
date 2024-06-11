1ec712fedffcf68ed08f85ef7a26e6cea043a3a8
Stian Gaustad <stian.gaustad@no.experis.com>
2024-06-11 10:54:25 +0200

    Header - Added logo image

--- src/components/header/LargeHeader/LargeHeader.tsx
+++ src/components/header/LargeHeader/LargeHeader.tsx
@@ -1,11 +1,12 @@
 import PageLinks from "./PageLinks/PageLinks";
 import UserOverview from "../UserOverview/UserOverview";
+import logo from "@/assets/logos/logo_relaxed_no_bg.png"
 
 const LargeHeader = () => {
     return(
         <>
             <div className="header-cinema-logo">
-                Logo placeholder
+                <img className="header-logo-image" src={logo}/>
             </div>
             <PageLinks />
             <UserOverview />
