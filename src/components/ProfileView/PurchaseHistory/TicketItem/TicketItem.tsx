2e3046b03814c18c17e438238ddc9406d2f5a0a5
Stian Gaustad <stian.gaustad@no.experis.com>
2024-06-12 10:48:05 +0200

    TicketItem - Implemented TicketItemMovie and TicketItemScreening components and expired styling

--- src/components/ProfileView/PurchaseHistory/TicketItem/TicketItem.tsx
+++ src/components/ProfileView/PurchaseHistory/TicketItem/TicketItem.tsx
@@ -1,5 +1,8 @@
 import { ITicket } from "@/interfaces/ITicket"
 import "./TicketItem.css"
+import { useEffect, useState } from "react";
+import TicketItemMovie from "./TicketItemMovie/TicketItemMovie";
+import TicketItemScreening from "./TicketItemScreening/TicketItemScreening";
 
 interface ITicketItemProps {
     ticket: ITicket,
@@ -7,19 +10,25 @@ interface ITicketItemProps {
 
 const TicketItem: React.FC<ITicketItemProps> = ({ticket}) => {
     const curDate = new Date();
+    const ticketStartTime = new Date(ticket.screening.startTime);
+    const [expired, setExpired] = useState<boolean>(false);
+
+    useEffect(() => {
+        setExpired(ticketStartTime > curDate);
+    }, [ticket])
+
     return(
-        <div className="ticket-item-parent-container top-rounded-corners highlight">
+        <div 
+            className={"ticket-item-parent-container top-rounded-corners clickable " + 
+                (expired ? "" : "expired")}
+        >
             <img 
                 className="ticket-item-card-image-header"
-                src="https://image.tmdb.org/t/p/original/hkxxMIGaiCTmrEArK7J56JTKUlB.jpg" 
+                src="https://preview.redd.it/l2zxnp3s6fec1.jpeg?auto=webp&s=62e4f74556328a9548cdaa21755fe571a8329c33" 
             />
-            <div className="">
-                <span>
-                    {ticket.screening.movie.title}
-                </span>
-                <span>
-                    {new Date(ticket.screening.startTime) > curDate ? "true" : "false"}
-                </span>
+            <div className="ticket-item-information-container">
+                <TicketItemMovie movie={ticket.screening.movie} />
+                <TicketItemScreening screening={ticket.screening} seat={ticket.seat} />
             </div>
         </div>
     )
