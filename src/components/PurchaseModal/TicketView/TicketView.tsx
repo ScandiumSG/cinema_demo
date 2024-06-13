200b851dca08fc67ba51e4acf3dd0e7e2abe9529
Stian Gaustad <stian.gaustad@no.experis.com>
2024-06-13 13:07:34 +0200

    TicketView - Implemented fetched TicketTypes and TicketType interfaces

--- src/components/PurchaseModal/TicketView/TicketView.tsx
+++ src/components/PurchaseModal/TicketView/TicketView.tsx
@@ -1,24 +1,23 @@
-import { ticketOptions } from "@/util/ticketUtils";
 import "./TicketView.css"
 import TicketItem from "./TicketItem/TicketItem";
-import { ITicketHandler } from "@/interfaces/ITicket";
+import { ITicketHandler, ITicketType } from "@/interfaces/ITicket";
 
 interface ITicketViewProps {
+    ticketOptions: ITicketType[],
     selectTickets: ITicketHandler,
     addTicket: (ticketType: string) => void,
     removeTicket: (ticketType: string) => void,
     setShowSeatMap: (value: boolean) => void,
 }
 
-const TicketView: React.FC<ITicketViewProps> = ({selectTickets, addTicket, removeTicket, setShowSeatMap}) => {
+const TicketView: React.FC<ITicketViewProps> = ({ticketOptions, selectTickets, addTicket, removeTicket, setShowSeatMap}) => {
     return(
         <>
         <div className="purchase-modal-ticket-options">
-                {ticketOptions.map((ticketGroup, index: number) => (
+                {ticketOptions.map((ticketType, index: number) => (
                     <TicketItem 
                     key={index} 
-                    itemCategory={ticketGroup.name}
-                    itemPrice={ticketGroup.price}
+                    ticketType={ticketType}
                     tickets={selectTickets}
                     increment={addTicket}
                     decrement={removeTicket}
