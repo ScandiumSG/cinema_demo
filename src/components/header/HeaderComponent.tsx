77d99d008f29bab2977e339341204e2170fe3b47
Stian Gaustad <stian.k.gaustad@gmail.com>
2024-11-05 21:17:51 +0100

    Header - Updated HeaderComponent to facilitate frontpage navigation

--- src/components/header/HeaderComponent.tsx
+++ src/components/header/HeaderComponent.tsx
@@ -1,50 +1,54 @@
 import { useContext, useEffect, useState } from "react";
 import LoginModal from "../common/loginModal/LoginModal";
-import "./HeaderComponent.css"
+import "./HeaderComponent.css";
 import { IUserContext } from "@/interfaces/UserInterfaces";
 import { headerNavigationContext, userContext } from "@/util/context";
 import SmallHeader from "./SmallHeader/SmallHeader";
 import LargeHeader from "./LargeHeader/LargeHeader";
+import { useNavigate } from "react-router-dom";
 
 const HeaderComponent = () => {
     const { loginModal } = useContext<IUserContext>(userContext);
     const [isDesktop, setDesktop] = useState<boolean>(window.innerWidth > 600);
-    const headerNavigationItems: {display: string, route: string}[] = [
-        //{display: "Frontpage", route: "/"}, 
-        {display: "Upcoming", route: "screening/upcoming"}, 
-        {display: "Movies", route: "movies"}
+    const navigate = useNavigate();
+    const headerNavigationItems: { display: string; route: string }[] = [
+        { display: "Frontpage", route: "/" },
+        { display: "Upcoming", route: "screening/upcoming" },
+        { display: "Movies", route: "movies" },
     ];
+
+    const routeToFrontpage = () => {
+        navigate("/");
+    };
 
     useEffect(() => {
         const handleWindowResize = () => {
             if (window.innerWidth > 600) {
                 setDesktop(true);
             } else {
-                setDesktop(false)
+                setDesktop(false);
             }
-        }
+        };
         window.addEventListener("resize", handleWindowResize);
 
         return () => {
             window.removeEventListener("resize", handleWindowResize);
-        }
-
-    }, [])
+        };
+    }, []);
 
     return (
-        <headerNavigationContext.Provider value={{
-            navItems: headerNavigationItems
-        }}>
+        <headerNavigationContext.Provider
+            value={{
+                navItems: headerNavigationItems,
+                toFrontpage: routeToFrontpage,
+            }}
+        >
             {loginModal && <LoginModal />}
             <header className="header-container">
-            {isDesktop ? 
-                <LargeHeader/>
-                : 
-                <SmallHeader/>
-            }
+                {isDesktop ? <LargeHeader /> : <SmallHeader />}
             </header>
         </headerNavigationContext.Provider>
-    )
-}
+    );
+};
 
 export default HeaderComponent;
